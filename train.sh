2eb378291397831356009fce67bb11850d0ae222
Farley Knight <farleyknight@gmail.com>
2022-09-03 22:10:58 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/zsh
+
+# Set up the machine for ML training
+
+## Install zsh and oh-my-zshell
+apt-get -y install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## Set up my email & name
+git config --global user.email "farleyknight@gmail.com"
+git config --global user.name "Farley Knight"
+
+## Install the proper Python libraries
+pip install wandb seqeval datasets evaluate
+pip install git+https://github.com/huggingface/transformers
 
